eed69c94f975cf61af5167264985edf6d68b2e83
Sean Jezewski <seanwjezewski@gmail.com>
2016-08-25 15:46:45 -0700

    Make portforwarding script a bit smart about how it kills processes

--- scripts/dm-portforwarding.sh
+++ scripts/dm-portforwarding.sh
@@ -4,16 +4,21 @@
 # or  30650 for pachctl
 
 PORT=$1
+FLAGS=$2
 
 if [ -z $PORT ]
 then
     PORT=8080
 fi
 
-ps -ef | grep "docker/machine" | grep -v grep | cut -f 4 -d " " | while read -r pid
+if [ -z $FLAGS ]
+then
+    FLAGS="-fTNL"
+fi
+
+ps -ef | grep "docker/machine" | grep ":$PORT" | grep -v grep | cut -f 4 -d " " | while read -r pid
 do
        kill $pid
 done
 
-docker-machine ssh dev -fTNL $PORT:localhost:$PORT
-
+docker-machine ssh dev $FLAGS $PORT:localhost:$PORT
